e0c65378fb1624912bfc6cc397c8907334cfb751
mwinowiecki <mwinowiecki@sigma.ug.edu.pl>
2013-04-22 18:36:02 +0300

    Update SKRYPT1.sql
    dodane max. wielkości łańcuchów znaków w tabelach. Prawidłowy insert w 2 wersjach

--- SKRYPT1.sql
+++ SKRYPT1.sql
@@ -1,28 +1,32 @@
---DROP TABLE adres_docelowy;
+--################### UTWORZENIE BAZY DANYCH ##########################
+
+CREATE DATABASE firma_spedycyjna;
+
+--####################### TWORZENIE TABEL #############################
 
 CREATE TABLE adres (
   idadres INT IDENTITY(1,1) PRIMARY KEY,
-  ulica VARCHAR NOT NULL,
-  nr_domu VARCHAR NOT NULL,
-  miejscowosc VARCHAR NOT NULL,
+  ulica VARCHAR(30) NOT NULL,
+  nr_domu VARCHAR(10) NOT NULL,
+  miejscowosc VARCHAR(30) NOT NULL,
   kod_poczt CHAR(6) NOT NULL,
 );
 
 CREATE TABLE adres_docelowy (
   idadres_docelowy INT IDENTITY(1,1) PRIMARY KEY,
-  imie VARCHAR NOT NULL,
-  nazwisko VARCHAR NOT NULL,
-  ulica VARCHAR NOT NULL,
-  nr_domu VARCHAR NOT NULL,
-  miejscowosc VARCHAR NOT NULL,
+  imie VARCHAR(20) NOT NULL,
+  nazwisko VARCHAR(30) NOT NULL,
+  ulica VARCHAR(30) NOT NULL,
+  nr_domu VARCHAR(10) NOT NULL,
+  miejscowosc VARCHAR(30) NOT NULL,
   kod_pocz CHAR(6) NOT NULL,
 );
 
 CREATE TABLE klient (
   idklient INT IDENTITY(1,1) PRIMARY KEY,
   idadres INT CONSTRAINT idadres_fk FOREIGN KEY(idadres) REFERENCES adres(idadres),
-  imie VARCHAR NOT NULL,
-  nazwisko VARCHAR NOT NULL,
+  imie VARCHAR(20) NOT NULL,
+  nazwisko VARCHAR(30) NOT NULL,
   PESEL CHAR(6) UNIQUE NOT NULL,
   NIP CHAR(10) UNIQUE
 );
@@ -42,14 +46,14 @@ CREATE TABLE typ_pojazdu (
 CREATE TABLE pojazd (
   idpojazd INT IDENTITY(1,1) PRIMARY KEY,
   idtyp_pojazdu INT CONSTRAINT idtyp_pojazdu_fk FOREIGN KEY(idtyp_pojazdu) REFERENCES typ_pojazdu(idtyp_pojazdu),
-  marka VARCHAR NOT NULL,
-  model VARCHAR NOT NULL,
+  marka VARCHAR(20) NOT NULL,
+  model VARCHAR(20) NOT NULL,
   nr_rej VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE stanowisko (
   idstanowisko INT IDENTITY(1,1) PRIMARY KEY,
-  stanowisko VARCHAR NOT NULL,
+  stanowisko VARCHAR(15) NOT NULL,
   pensja MONEY NOT NULL,
   opis VARCHAR(50) CHECK(LEN(opis)>10) NOT NULL,
 );
@@ -59,8 +63,8 @@ CREATE TABLE pracownik (
   idfilia INT CONSTRAINT idfilia_fk FOREIGN KEY(idfilia) REFERENCES filia(idfilia),
   idadres INT CONSTRAINT idadres_pracow_fk FOREIGN KEY(idadres) REFERENCES adres(idadres),
   idstanowisko INT CONSTRAINT idstanowisko_fk FOREIGN KEY(idstanowisko) REFERENCES stanowisko(idstanowisko),
-  imie VARCHAR NOT NULL,
-  nazwisko VARCHAR NOT NULL,
+  imie VARCHAR(20) NOT NULL,
+  nazwisko VARCHAR(30) NOT NULL,
   PESEL CHAR(6) UNIQUE NOT NULL,
 );
 
@@ -74,48 +78,32 @@ CREATE TABLE faktura (
   stawka_VAT FLOAT NOT NULL,
   cena_razem_brutto MONEY NOT NULL,
   data_wyjazdu DATETIME NOT NULL,
-  data_przyjazdu DATETIME NOT NULL
+  data_przyjazdu DATETIME NOT NULL,
+  CONSTRAINT spr_date CHECK(data_przyjazdu>=data_wyjazdu)
 );
-
-SELECT GETDATE();
-
-ALTER TABLE faktura ADD CONSTRAINT sprawdz_date CHECK(data_przyjazdu>=data_wyjazdu);
 
 CREATE TABLE zamowienie (
   idpojazd INT CONSTRAINT idpojazd_fk FOREIGN KEY(idpojazd) REFERENCES pojazd(idpojazd),
   idfaktura INT CONSTRAINT idfaktura_fk FOREIGN KEY(idfaktura) REFERENCES faktura(idfaktura)
 );
 
+--######################### WPISYWANIE DANYCH #########################
 
---#####################################################################################
+INSERT INTO adres(ulica,nr_domu,miejscowosc,kod_poczt) 
+VALUES ('Sloneczna','4b/15','Sadki','56-632');
 
-
-SET IDENTITY_INSERT faktura ON;
-
-INSERT INTO faktura(idfaktura,idadres_docelowy,idpracownik,idklient,dystans_km,cena_razem_netto,stawka_VAT,cena_razem_brutto,data_wyjazdu,data_przyjazdu) 
-VALUES (2,NULL,NULL,NULL,10,15,0.23,15*1.23,'2005-10-13','2005-10-15');
-
-SET IDENTITY_INSERT faktura OFF;
-
-SELECT * FROM faktura;
-
-DELETE FROM faktura;
---SELECT 'ALTER TABLE ' + so.TABLE_NAME + ' DROP CONSTRAINT ' + so.CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS so; 
-
---ALTER TABLE faktura DROP CONSTRAINT PK__faktura__0065E14F43F60EC8;
---ALTER TABLE faktura DROP CONSTRAINT CK__faktura__dystans__48BAC3E5;
-
---ALTER TABLE faktura DROP CONSTRAINT idadr_doc_fk;
---ALTER TABLE faktura DROP CONSTRAINT idpracownik_fk;
---ALTER TABLE faktura DROP CONSTRAINT idklient_fk;
-
-DROP TABLE faktura;
+INSERT INTO adres VALUES ('Poziomkowa','5','Czersk','89-560');
 
 
+--######################## WYSWIETLENIE TABEL #########################
 
---ALTER TABLE zamowienie DROP CONSTRAINT idfaktura_fk;
---ALTER TABLE zamowienie ADD CONSTRAINT idfaktura_fk FOREIGN KEY(idfaktura) REFERENCES faktura(idfaktura);
-
+SELECT * FROM adres;
+SELECT * FROM adres_docelowy;
 SELECT * FROM klient;
+SELECT * FROM filia;
+SELECT * FROM typ_pojazdu;
+SELECT * FROM pojazd;
+SELECT * FROM stanowisko;
 SELECT * FROM pracownik;
 SELECT * FROM faktura;
+SELECT * FROM zamowienie;
