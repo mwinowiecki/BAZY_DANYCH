aeb5c4fd50b3825de5fa59acb891647b846146d4
mwinowiecki <mwinowiecki@sigma.ug.edu.pl>
2013-04-22 22:09:22 +0300

    Update SKRYPT1.sql
    ZMIENIONE ROZMIARY PESEL'i NA 11 ZNAKOW

--- SKRYPT1.sql
+++ SKRYPT1.sql
@@ -27,7 +27,7 @@ CREATE TABLE klient (
   idadres INT CONSTRAINT idadres_fk FOREIGN KEY(idadres) REFERENCES adres(idadres),
   imie VARCHAR(20) NOT NULL,
   nazwisko VARCHAR(30) NOT NULL,
-  PESEL CHAR(6) UNIQUE NOT NULL,
+  PESEL CHAR(11) UNIQUE NOT NULL,
   NIP CHAR(10) UNIQUE
 );
 
@@ -48,7 +48,7 @@ CREATE TABLE pojazd (
   idtyp_pojazdu INT CONSTRAINT idtyp_pojazdu_fk FOREIGN KEY(idtyp_pojazdu) REFERENCES typ_pojazdu(idtyp_pojazdu),
   marka VARCHAR(20) NOT NULL,
   model VARCHAR(20) NOT NULL,
-  nr_rej VARCHAR UNIQUE NOT NULL
+  nr_rej VARCHAR(10) UNIQUE NOT NULL
 );
 
 CREATE TABLE stanowisko (
@@ -65,7 +65,7 @@ CREATE TABLE pracownik (
   idstanowisko INT CONSTRAINT idstanowisko_fk FOREIGN KEY(idstanowisko) REFERENCES stanowisko(idstanowisko),
   imie VARCHAR(20) NOT NULL,
   nazwisko VARCHAR(30) NOT NULL,
-  PESEL CHAR(6) UNIQUE NOT NULL,
+  PESEL CHAR(11) UNIQUE NOT NULL,
 );
 
 CREATE TABLE faktura (
@@ -89,12 +89,34 @@ CREATE TABLE zamowienie (
 
 --######################### WPISYWANIE DANYCH #########################
 
-INSERT INTO adres(ulica,nr_domu,miejscowosc,kod_poczt) 
-VALUES ('Sloneczna','4b/15','Sadki','56-632');
+--### TAB adres ###
+INSERT INTO adres VALUES ('SLONECZNA','4B/15','SADKI','89-110');
+INSERT INTO adres VALUES ('POZIOMKOWA','5','CZERSK','89-560');
+INSERT INTO adres VALUES ('MALINOWA','8','CHOJNICE','89-600');
+INSERT INTO adres VALUES ('BOHATEROW WESTERPLATTE','3','TUCHOLA','89-501');
+INSERT INTO adres VALUES ('CEGIELNIANA','6A/8','TUCHOLA','89-501');
+INSERT INTO adres VALUES ('KORONOWSKA','9','BYDGOSZCZ','85-405');
 
-INSERT INTO adres VALUES ('Poziomkowa','5','Czersk','89-560');
 
+--### TAB adres_docelowy
+INSERT INTO adres_docelowy VALUES ('JAN','KOWALSKI','GRUNWALDZKA','146','GDANSK','80-264');
+INSERT INTO adres_docelowy VALUES ('MAREK','MARECKI','ULICZNA','23','WARSZAWA','05-077');
+INSERT INTO adres_docelowy VALUES ('ANNA','NOWAK','DWORCOWA','2','SZCZECINEK','78-410');
+INSERT INTO adres_docelowy VALUES ('PELAGIA','STOLARZ','KONSTYTUCJI 3 MAJA','10','TORUN','87-120');
+INSERT INTO adres_docelowy VALUES ('GENOWEFA','PIGWA','AKACJOWA','5','KRAKOW','31-466');
 
+--### TAB klient
+INSERT INTO klient VALUES (1,'STEFAN','BURCZYMUCHA','90052443528','5934168314');
+INSERT INTO klient VALUES (2,'KUNEGUNDA','TRAWKA','64111236829','6854235129');
+INSERT INTO klient VALUES (3,'JADWIGA','KONIECZNA','53052945865','5746325684');
+INSERT INTO klient VALUES (4,'MARIANNA','WELON','86012363547','5712365485');
+INSERT INTO klient VALUES (5,'ZENON','INTERES','70022636894','9645232156');
+
+--### TAB filia
+INSERT INTO filia VALUES (6);
+INSERT INTO filia VALUES (3);
+
+--update adres set miejscowosc=UPPER(miejscowosc);
 --######################## WYSWIETLENIE TABEL #########################
 
 SELECT * FROM adres;
@@ -107,3 +129,6 @@ SELECT * FROM stanowisko;
 SELECT * FROM pracownik;
 SELECT * FROM faktura;
 SELECT * FROM zamowienie;
+
+--SELECT * FROM filia JOIN adres ON filia.idadres=adres.idadres;
+--DBCC CHECKIDENT ( adres,RESEED,0);
