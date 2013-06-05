919af584b9ee299f309d5d125672c5f567e53c2a
mwinowiecki <mwinowiecki@sigma.ug.edu.pl>
2013-06-05 15:40:45 +0300

    Update skrypt_psql.sql

--- skrypt_psql.sql
+++ skrypt_psql.sql
@@ -5,15 +5,16 @@
 CREATE DATABASE firma_sped;
 
 DROP TABLE IF EXISTS zamowienie CASCADE;
-DROP TABLE IF EXISTSfaktura CASCADE;
+DROP TABLE IF EXISTS faktura CASCADE;
 DROP TABLE IF EXISTS pojazd CASCADE;
 DROP TABLE IF EXISTS typ_pojazdu CASCADE;
 DROP TABLE IF EXISTS pracownik CASCADE;
 DROP TABLE IF EXISTS filia CASCADE;
-DROP TABLE IF EXISTSstanowisko CASCADE;
+DROP TABLE IF EXISTS stanowisko CASCADE;
 DROP TABLE IF EXISTS adres_docelowy CASCADE;
 DROP TABLE IF EXISTS klient CASCADE;
 DROP TABLE IF EXISTS adres CASCADE;
+
 
 CREATE TABLE adres (
   idadres serial PRIMARY KEY,
@@ -152,6 +153,11 @@ INSERT INTO klient VALUES (3,3,'JADWIGA','KONIECZNA','53052945865','5746325684')
 INSERT INTO klient VALUES (4,4,'MARIANNA','WELON','86012363547','5712365485');
 INSERT INTO klient VALUES (5,5,'ZENON','INTERES','70022636894','9645232156');
 
+--###  TAB filia
+
+INSERT INTO filia VALUES(1,6);
+INSERT INTO filia VALUES(2,3);
+
 --### TAB typ_pojazdu
 INSERT INTO typ_pojazdu VALUES (1,'BUS',1,'PRZEWOZ DO 8 OSOB');
 INSERT INTO typ_pojazdu VALUES (2,'DOSTAWCZY',2.5,'PRZEWOZ TOWAROW');
@@ -261,7 +267,7 @@ DROP FUNCTION IF EXISTS akt_klienta(INT);
 CREATE FUNCTION akt_klienta(id_klient INT) 
 RETURNS INTEGER AS $$
 BEGIN
-  RETURN (SELECT COUNT(*) FROM klient k	JOIN faktura f 
+  RETURN (SELECT COUNT(*) FROM klient k JOIN faktura f 
   ON k.idklient=f.idklient WHERE k.idklient=id_klient);
 END;
 $$
@@ -308,7 +314,7 @@ CREATE FUNCTION suma_km(idp INT)
 RETURNS FLOAT 
 AS $$  
 DECLARE suma FLOAT;
-BEGIN  	
+BEGIN   
   suma:=0;
   SELECT (f.dystans_km) INTO suma FROM pracownik p JOIN faktura f
   ON p.idpracownik=f.idpracownik WHERE f.idpracownik=idp;
@@ -325,7 +331,7 @@ SELECT suma_km(1) as SUMA_KILOMETROW;
 -- PROCEDURA DODAWANIA NOWEGO PRACOWNIKA (NIE TRZEBA PAMIETAC id OSTATNIEGO ADRESU W TABELI adres)
 -- NALEZY PODAC DANE W ODPOWIEDNIEJ KOLEJNOSCI, A PROCEDURA SAMA ZADBA O POPRAWNE DODANIE REKORDOW
 
-DROP FUNCTION IF EXISTS dodaj_prac;
+DROP FUNCTION IF EXISTS dodaj_prac(INT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,INT,INT);
 
 CREATE FUNCTION dodaj_prac (INT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,INT,INT) RETURNS void AS
 $$
@@ -347,386 +353,4 @@ BEGIN
         VALUES (idprac,idfilia1,idadres1,imie1,nazw1,pesel1);
 END;
 $$ LANGUAGE 'plpgsql';
-
-SELECT dodaj_prac(5,'MARCIN','WOLNY','84563654598','SZYBKA','15C','BLISKIE','66-856',2,3);
-
-SELECT * FROM pracownik;
-
-
-
---8) procedura 2
--- NOWE ZAMOWIENIE (TWORZENIE NOWEJ FAKTURY)
-IF EXISTS(SELECT * FROM SYS.objects WHERE type='P' AND 
-
-name='nowe_zamowienie')
-DROP PROCEDURE nowe_zamowienie;
-GO
-
-CREATE PROCEDURE nowe_zamowienie
-	@cel_imie VARCHAR(20), 
-
-@cel_naz VARCHAR(30), @cel_ul VARCHAR(30), @cel_nr VARCHAR(10), @cel_miej VARCHAR(30), @cel_kodp CHAR(6),
-	@idprac 
-
-INT, @k_imie VARCHAR(20), @k_naz VARCHAR(30), @k_pesel CHAR(11), @k_nip CHAR(10), @k_ulica VARCHAR(30), 
-	@k_nr 
-
-VARCHAR(10), @k_miej VARCHAR(30), @k_kodp CHAR(6),
-	@dystans INT, @cena FLOAT, @vat FLOAT, @data_wyj DATETIME, 
-
-@data_przyj DATETIME,
-	@idpoj INT	
-AS
-	INSERT INTO adres_docelowy VALUES 
-
-(@cel_imie,@cel_naz,@cel_ul,@cel_nr,@cel_miej,@cel_kodp)
-	DECLARE @max_adr_doc INT
-	SET @max_adr_doc = (SELECT 
-
-MAX(idadres_docelowy) FROM adres_docelowy)
-
-	INSERT INTO adres VALUES (@k_ulica,@k_nr,@k_miej,@k_kodp)
-
-	DECLARE 
-
-@max_k_adr INT
-	SET @max_k_adr=(SELECT MAX(idadres) FROM adres)	
-	INSERT INTO klient VALUES 
-
-(@max_k_adr,@k_imie,@k_naz,@k_pesel,@k_nip)
-
-	DECLARE @max_id_k INT
-	SET @max_id_k = (SELECT MAX(idklient) FROM klient)	
-
-	INSERT INTO faktura VALUES (@max_adr_doc,@idprac,@max_id_k,@dystans,@cena,@vat,@cena*(@vat
-
-+1),@data_wyj,@data_przyj)
-
-	DECLARE @max_id_fak INT
-	SET @max_id_fak = (SELECT MAX(idfaktura) FROM faktura)
-
-	INSERT INTO 
-
-zamowienie VALUES (@idpoj,@max_id_fak)
-GO
-
-EXECUTE nowe_zamowienie 'ZENON','BIEDRONKA','PILSKA','10/3','ILAWA','80-
-
-632',5,'MICHAL','OJCZENASZ',
-		84032512532,NULL,'CEGIELNIANA','4E/55','TUCHOLA','89-501',260,400,0.23,'2010-10-
-
-23','2010-10-25',3
-GO
-
-
-
-
-
---9) procedura 3
---DODAWANIE NOWEGO SAMOCHODU
-IF EXISTS (SELECT * FROM sys.objects WHERE type='P' AND name='dodaj_sam')
-DROP 
-
-PROCEDURE dodaj_sam;
-GO
-
-CREATE PROCEDURE dodaj_sam
-	@marka VARCHAR(20), @model VARCHAR(20), @nr_rej VARCHAR(10), @typ 
-
-VARCHAR(20), @cena_netto_km MONEY, 
-	@opis VARCHAR(50)
-AS
-	INSERT INTO typ_pojazdu VALUES (@typ,@cena_netto_km,@opis)
-
-	
-
-DECLARE @max_idtyp INT
-	SET @max_idtyp = (SELECT MAX(idtyp_pojazdu) FROM typ_pojazdu)
-
-	INSERT INTO pojazd VALUES 
-
-(@max_idtyp,@marka,@model,@nr_rej)
-GO
-
-EXECUTE dodaj_sam 'FIAT','DUCATO','WE 62345','DOSTAWCZY',6.5,'POJAZD O DMC 3,5T'
-GO
-
-
-
-
-
---10) procedura 4
---ZWIEKSZANIE PENSJI NA DANYM STANOWISKU (O JAKIS PROCENT)
-IF EXISTS (SELECT * FROM sys.objects WHERE 
-
-type='P' AND name='zwieksz_pensje')
-DROP PROCEDURE zwieksz_pensje;
-GO
-
-CREATE PROCEDURE zwieksz_pensje
-	@id_stan INT, 
-
-@ile_proc FLOAT
-AS
-	DECLARE @proc FLOAT
-	SET @proc=(@ile_proc/100)+1
-	UPDATE stanowisko SET 
-
-pensja_netto=pensja_netto*@proc WHERE idstanowisko=@id_stan
-GO
-
-SELECT * FROM stanowisko;
-EXECUTE zwieksz_pensje 3,10.0;	-- 
-
-ZWIEKSZAMY PENSJE NA STANOWISKU O id = 2 O 10 PROCENT 
-SELECT * FROM stanowisko;
-
-
-
-
---11) wyzwalacz 1
--- WYSWIETLA DANE OSTATNIO DODANEGO SAMOCHODU (PLUS WYKORZYSTANIE WCZESNIEJ NAPISANEJ PROCEDURY)
-IF EXISTS 
-
-(SELECT * FROM sys.objects WHERE type='TR' AND name='dodano_samochod')
-DROP TRIGGER dodano_samochod;
-GO
-
-CREATE TRIGGER 
-
-dodano_samochod ON pojazd
-AFTER INSERT
-AS
-BEGIN
-	DECLARE @id INT
-	SET @id = (SELECT MAX(idpojazd) FROM pojazd)
-
-	SELECT 
-
-'DODANO NOWY SAMOCHOD!'
-	SELECT * FROM pojazd WHERE idpojazd=@id
-END
-GO
-
-EXECUTE dodaj_sam 'RENAULT','KANGOO','WE 
-
-12852','OSOBOWY',1.8,'PRZEWOZ OSOB I DROBNYCH TOWAROW'
-GO
-
-
-
-
---12) wyzwalacz 2
-
---WYZWALACZ TEN "PILNUJE", ABYSMY NIE ZDUBLOWALI KTOREGOS Z ADRESOW DOCELOWYCH
-IF EXISTS (SELECT * FROM sys.objects WHERE 
-
-type='TR' AND name='duplikat_adresu_doc')
-DROP TRIGGER duplikat_adresu_doc;
-GO
-
-CREATE TRIGGER duplikat_adresu_doc ON 
-
-adres_docelowy
-INSTEAD OF INSERT
-AS
-BEGIN
-	DECLARE @imie VARCHAR(20), @naz VARCHAR(30), @ul VARCHAR(30), @nr VARCHAR(10), 
-
-@miej VARCHAR(30), @kod CHAR(6)
-
-	SELECT @imie=imie, @naz=nazwisko, @ul=ulica, @nr=nr_domu, 
-		@miej=miejscowosc, 
-
-@kod=kod_pocz FROM inserted
-
-	SET NOCOUNT ON
-	IF NOT EXISTS (
-	SELECT *
-	FROM adres_docelowy a, inserted i
-	
-
-WHERE (a.imie=i.imie) AND (a.nazwisko=i.nazwisko) AND (a.ulica=i.ulica) AND (a.nr_domu=i.nr_domu) 
-	AND 
-
-(a.miejscowosc=i.miejscowosc) AND (a.kod_pocz=i.kod_pocz))
-	BEGIN
-		INSERT INTO adres_docelowy VALUES 
-
-(@imie,@naz,@ul, @nr, @miej, @kod)
-	END
-	ELSE
-		BEGIN
-			RAISERROR('NIE MOZNA DODAC NOWEGO 
-
-ADRESU! ADRES JUZ ISTNIEJE...',1,2)	
-			ROLLBACK
-		END		
-END
-GO
-
-INSERT INTO 
-
-adres_docelowy VALUES('ZENON','BIEDRONKA','PILSKA','10/3','ILAWA','80-632')
-
-
-
-
---13) wyzwalacz 3
-
---WYZWALACZ WYSWIETLAJACY ZMODYFIKOWANE WIERSZE PO ZMIANIE DANYCH KLIENTA
---DZIEKI NIEMU WIEMY CO SIE ZMIENILO I Z CZEGO, 
-
-WIEC NIE BEDZIE 
-IF EXISTS (SELECT * FROM sys.objects WHERE type='TR' AND name = 'zmiana_danych')
-DROP TRIGGER 
-
-zmiana_danych;
-GO
-
-CREATE TRIGGER zmiana_danych ON klient
-AFTER INSERT, UPDATE, DELETE
-AS
-BEGIN
-	SELECT 'ZMODYFIKOWANO 
-
-WIERSZY: '+STR(@@ROWCOUNT)
-	SELECT 'USUNIETO WIERSZE: '
-	SELECT * FROM deleted
-	SELECT 'DODANO WIERSZE: '
-	
-
-SELECT * FROM inserted
-END
-GO
-
-UPDATE klient SET imie='WOJTEK' WHERE idklient=1;
-
-
-
---14) wyzwalacz 4
-
---WYZWALACZ TWORZACY "KOPIE ZAPASOWA" USUNIETYCH REKORDOW Z TABELI pracownik  W TABELI pracownik_kopia
-IF EXISTS (SELECT * 
-
-FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='pracownik_kopia')
-DROP TABLE pracownik_kopia;
-GO
-
-
-
-CREATE TABLE pracownik_kopia (
-  idpracownik INT IDENTITY(1,1) PRIMARY KEY,
-  idfilia INT CONSTRAINT idfilia_fk2 FOREIGN KEY
-
-(idfilia) REFERENCES filia(idfilia),
-  idadres INT CONSTRAINT idadres_pracow_fk2 FOREIGN KEY(idadres) REFERENCES adres
-
-(idadres),
-  idstanowisko INT CONSTRAINT idstanowisko_fk2 FOREIGN KEY(idstanowisko) REFERENCES stanowisko(idstanowisko),
-  
-
-imie VARCHAR(20) NOT NULL,
-  nazwisko VARCHAR(30) NOT NULL,
-  PESEL CHAR(11) CONSTRAINT unikalny_pesel2 UNIQUE NOT NULL,
-);
-
-IF 
-
-EXISTS (SELECT * FROM sys.objects WHERE type='TR' AND name='kopia_zapas')
-DROP TRIGGER kopia_zapas;
-GO
-
-CREATE TRIGGER 
-
-kopia_zapas ON pracownik
-AFTER DELETE
-AS
-BEGIN
-	SET IDENTITY_INSERT pracownik_kopia ON
-	ALTER TABLE pracownik_kopia DROP 
-
-CONSTRAINT unikalny_pesel2
-	IF EXISTS (SELECT * FROM deleted)
-	BEGIN
-		INSERT INTO pracownik_kopia 
-
-(idpracownik,idfilia,idadres,idstanowisko,imie,nazwisko,PESEL)
-		SELECT * FROM deleted
-	END
-	ALTER TABLE 
-
-pracownik_kopia ADD CONSTRAINT unikalny_pesel2 UNIQUE (PESEL)
-	SET IDENTITY_INSERT pracownik_kopia OFF
-END
-GO
-
-SELECT * FROM 
-
-pracownik;
-DELETE FROM pracownik WHERE idpracownik = 3;
-SELECT * FROM pracownik_kopia;
-
---15) pivot 1
---TABELA PRZESTAWNA 
-
-UKAZUJACA PRZYCHOD DANEGO KIEROWCY W WYBRANYCH LATACH LATACH
-SELECT p.idpracownik, p.imie, p.nazwisko, [2009],[2010]
-FROM 
-(
-	
-
-SELECT p.idpracownik, p.imie, p.nazwisko, YEAR(data_wyjazdu) AS przychod_pracow, cena_razem_brutto
-	FROM faktura f JOIN 
-
-pracownik p ON f.idpracownik=p.idpracownik
-) tabela
-PIVOT
-(
-	SUM(cena_razem_brutto)
-	FOR przychod_pracow IN ([2009],
-
-[2010])
-)
-AS p
-ORDER BY idpracownik;
-
-SELECT * FROM faktura;
-
---16) pivot 2
---TABELA PRZESTAWNA UKAZUJACA ILE KILOMERTOW W ROKU 
-
-PRZEJECHAL DANY POJAZD
-
-EXECUTE nowe_zamowienie 'ANDRZEJ','SLABY','KROTKA','2','POZNAN','25-522',4,'PIOTR','KROL',
-		
-
-84031512862,6325451685,'DLUGA','30','SZCZECIN','36-451',365,426.55,0.23,'2009-03-10','2009-03-12',2
-GO
-
-SELECT p.idpojazd, 
-
-p.marka, p.model, p.nr_rej, [2009], [2010]
-FROM 
-(
-	SELECT p.idpojazd, p.marka, p.model, p.nr_rej, YEAR(f.data_wyjazdu) 
-
-AS suma_km, dystans_km 
-	FROM 
-	faktura f JOIN zamowienie z ON f.idfaktura=z.idfaktura
-	JOIN pojazd p ON 
-
-z.idpojazd=p.idpojazd
-) tabela
-PIVOT
-(
-	SUM(dystans_km)
-	FOR suma_km IN ([2009],[2010])
-)
-AS p
-ORDER BY idpojazd;
-
-SELECT * FROM 
-
-faktura;
 
